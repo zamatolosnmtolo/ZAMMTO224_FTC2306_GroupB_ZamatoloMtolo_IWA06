794ff790ec51db56eedf88440a4ffa1ccef57c96
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-08-11 10:08:34 +0200

    Update JS1

--- Challenge 1/scripts.js
+++ Challenge 1/scripts.js
@@ -3,8 +3,8 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number == secondaryPhone
+const primaryValid = /^[0-9]+$/.test(primaryPhone);
+const secondaryValid = /^[0-9]+$/.test(secondaryPhone);
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
